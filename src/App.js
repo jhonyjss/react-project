892a76463028f3247450cac27129b464604055e1
Jhony Souza <jhony.jss@gmail.com>
2021-10-26 13:08:20 -0300

    chore: aula 3

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+  // não precisa utilizar o construtor com public fields
+  state = {
+    name: 'Jhony Souza',
+    counter: 0
+  }
+  
+  // Através das arrow functions você não precisa criar o bind do método
+  handlePClick = (event) => {
+    this.setState({name: 'Emily Souzza'})
+  }
+  
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state
+    const nextCounter = counter + 1
+    this.setState({counter: nextCounter })
+  }
+  
   render() {
+    const { name, counter } = this.state
     return (
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={this.handlePClick}>
+          {name} - {counter}
         </p>
         <a
+          onClick={this.handleAClick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
