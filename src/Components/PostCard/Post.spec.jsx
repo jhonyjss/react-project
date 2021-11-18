57106d6e4285658e5aa2b6e076d06156b4a8f1fa
Jhony Souza <jhony.jss@gmail.com>
2021-11-18 14:06:59 -0300

    test: testing components buttons, posts and creating snapshots

---
+++ src/Components/PostCard/Post.spec.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { PostCard } from ".";
+import { postCardPropsMock } from "./mock";
+
+const props = postCardPropsMock;
+describe("<PostCard />", () => {
+  it("should render PostCard correctly", () => {
+    render(<PostCard {...props} />);
+    expect(screen.getByAltText(props.title)).toHaveAttribute(
+      "src",
+      props.cover
+    );
+
+    expect(
+      screen.getByRole("heading", { name: props.title })
+    ).toBeInTheDocument();
+
+    expect(screen.getByText("body")).toBeInTheDocument();
+  });
+
+  it("should match snapshot", () => {
+    const { container } = render(<PostCard {...props} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
